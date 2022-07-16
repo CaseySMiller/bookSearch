3270d41b27df3ea951c616338e7cf254a58cfbf5
CaseySMiller <CaseySMiller@yahoo.com>
2022-07-16 12:26:30 -0600

    fixed pathing middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-require('dotenv').config();
+require('dotenv').config({ path: './.env' });
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -17,13 +17,13 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+console.log(process.env.NODE_ENV);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
